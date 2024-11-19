22746a7919a832136f15db44a9ab8109e0c91f2c
Esteban Durango <esteban.durangov@udea.edu.co>
2024-11-19 11:32:15 -0500

    Enhance mobility API to include security scopes for current user and remove debug print statement

--- app/api/routes/v1/application/type/mobility.py
+++ app/api/routes/v1/application/type/mobility.py
@@ -1,66 +1,74 @@
+from __future__ import annotations
+
 from datetime import datetime
 from typing import Annotated
-from fastapi import APIRouter, Depends, HTTPException
 from uuid import UUID
+
+from fastapi import APIRouter
+from fastapi import Depends
+from fastapi import HTTPException
+from fastapi import Security
 from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
 
 from app.api.middleware.bearer import get_current_active_user
-from app.schemas.application.type.mobility import MobilityCreate, Mobility, MobilityUpdate
-
-from app.schemas.application.user_application import UserApplicationCreate
-from app.protocols.db.models.application.application import ApplicationStatusType
-
-
-from app.schemas.application.user_application import UserApplicationStatus
-from app.schemas.users.user import User
-
-from app.services.application.type.mobility import mobility_svc
-from app.services.application.user_application import user_application_svc
-
 from app.api.middleware.mongo_db import get_mongo_db
 from app.api.middleware.postgres_db import get_db
-
+from app.infraestructure.policies.application.type.mobility import flux
+from app.infraestructure.policies.application.type.mobility import next_status
+from app.infraestructure.policies.application.user_application import current_status
 from app.protocols.db.models.application.application import ApplicationStatusType
-
+from app.schemas.application.type.mobility import Mobility
+from app.schemas.application.type.mobility import MobilityCreate
+from app.schemas.application.type.mobility import MobilityUpdate
+from app.schemas.application.user_application import UserApplicationCreate
+from app.schemas.application.user_application import UserApplicationStatus
 from app.schemas.application.user_application_academic_unit import UserApplicationAcademicUnitCreate
-from app.services.application.user_application_academic_unit import user_application_academic_unit_svc
-
+from app.schemas.users.user import User
 from app.schemas.voting.voting import VotingCreate
+from app.services.application.type.mobility import mobility_svc
+from app.services.application.user_application import user_application_svc
+from app.services.application.user_application_academic_unit import user_application_academic_unit_svc
 from app.services.voting.voting import voting_svc
 
 
 router = APIRouter()
 
-@router.post("/create", response_model=MobilityCreate, status_code=201)
-async def create_mobility(*, 
-                          obj_in: MobilityCreate, 
-                          db_mongo = Depends(get_mongo_db), 
-                          db_postgres: Session = Depends(get_db),
-                          current_user: Annotated[User, Depends(get_current_active_user)]
-                          ) -> MobilityCreate:
+
+@router.post('/create', response_model=MobilityCreate, status_code=201)
+async def create_mobility(
+    *,
+    obj_in: MobilityCreate,
+    db_mongo=Depends(get_mongo_db),
+    db_postgres: Session = Depends(get_db),
+    current_user: Annotated[
+        User, Security(
+            get_current_active_user, scopes=[
+                'estudiante:posgrado', 'estudiante:pregrado',
+            ],
+        ),
+    ] = None,
+) -> MobilityCreate:
 
     mobility_create = await mobility_svc.create(
         db_mongo=db_mongo,
         obj_in=obj_in,
         db_postgres=db_postgres,
         current_user=current_user,
-        application_id="1c779ce5-ce77-49ea-87e2-69a2388e53f2"
+        application_id='1c779ce5-ce77-49ea-87e2-69a2388e53f2',
     )
-
 
     return mobility_create
 
-from app.infraestructure.policies.application.user_application import current_status
-from app.infraestructure.policies.application.type.mobility import next_status, flux
 
-@router.patch("/update/{id}", response_model=str, status_code=200)
-async def update_status(*, 
-                          id: UUID,
-                          db_mongo = Depends(get_mongo_db), 
-                          db_postgres: Session = Depends(get_db),
-                          current_user: Annotated[User, Depends(get_current_active_user)]
-                          ) -> Mobility:
+@router.patch('/update/{id}', response_model=str, status_code=200)
+async def update_status(
+    *,
+    id: UUID,
+    db_mongo=Depends(get_mongo_db),
+    db_postgres: Session = Depends(get_db),
+    current_user: Annotated[User, Depends(get_current_active_user)],
+) -> Mobility:
 
     await flux(user_application_id=id, db_mongo=db_mongo, db_postgres=db_postgres, current_user=current_user)
-    return JSONResponse(content="Status updated", status_code=200)
+    return JSONResponse(content='Status updated', status_code=200)
--- app/infraestructure/policies/application/type/mobility.py
+++ app/infraestructure/policies/application/type/mobility.py
@@ -1,10 +1,14 @@
-from datetime import datetime
-from app.protocols.db.models.application.application import ApplicationStatusType
-from app.infraestructure.policies.application.user_application import current_status, send_to_academic_unit, create_voting
-from app.schemas.application.user_application import UserApplicationStatus
-from app.services.application.type.mobility import mobility_svc
-from app.schemas.users.user import User
+from __future__ import annotations
 
+from datetime import datetime
+
+from app.infraestructure.policies.application.user_application import create_voting
+from app.infraestructure.policies.application.user_application import current_status
+from app.infraestructure.policies.application.user_application import send_to_academic_unit
+from app.protocols.db.models.application.application import ApplicationStatusType
+from app.schemas.application.user_application import UserApplicationStatus
+from app.schemas.users.user import User
+from app.services.application.type.mobility import mobility_svc
 from app.services.users.user_rol_academic_unit import user_rol_academic_unit_svc
 
 
@@ -12,43 +16,50 @@ def next_status(current_status: str, response: str | None = None) -> str:
     if current_status == ApplicationStatusType.CREATE.value:
         return ApplicationStatusType.IN_COMMITEE.value
     elif current_status == ApplicationStatusType.IN_COMMITEE.value:
-        if (response == "APROBADA"):
+        if (response == 'APROBADA'):
             return ApplicationStatusType.IN_INTERNATIONAL.value
-        elif (response == "RECHAZADA"):
+        elif (response == 'RECHAZADA'):
             return ApplicationStatusType.RETURNED.value
-        else: return ApplicationStatusType.RETURNED.value
+        else:
+            return ApplicationStatusType.RETURNED.value
     elif current_status == ApplicationStatusType.IN_INTERNATIONAL.value:
-        if (response == "APROBADA"):
+        if (response == 'APROBADA'):
             return ApplicationStatusType.IN_DEAN.value
-        elif (response == "RECHAZADA"):
+        elif (response == 'RECHAZADA'):
             return ApplicationStatusType.RETURNED.value
     elif current_status == ApplicationStatusType.IN_DEAN.value:
-        if (response == "APROBADA"):
+        if (response == 'APROBADA'):
             return ApplicationStatusType.APPROVED.value
-        elif (response == "RECHAZADA"):
+        elif (response == 'RECHAZADA'):
             return ApplicationStatusType.RETURNED.value
     else:
         return None
-    
-async def flux(*, user_application_id, db_mongo, db_postgres, current_user:User) -> str:
+
+
+async def flux(*, user_application_id, db_mongo, db_postgres, current_user: User) -> str:
     _current_status = await current_status(user_application_id, db_mongo, mobility_svc)
     _next_status = next_status(_current_status)
-    committee = user_rol_academic_unit_svc.get_student_committee(user_id=current_user.id, db=db_postgres)
-
-    print(_next_status)
+    committee = user_rol_academic_unit_svc.get_student_committee(
+        user_id=current_user.id, db=db_postgres,
+    )
 
     if _next_status == ApplicationStatusType.IN_COMMITEE.value:
-        send_to_academic_unit(academic_unit_id=committee, user_application_id=user_application_id, db=db_postgres)
+        send_to_academic_unit(
+            academic_unit_id=committee,
+            user_application_id=user_application_id, db=db_postgres,
+        )
         await create_voting(academic_unit_id=committee, user_application_id=user_application_id, db_postgres=db_postgres, db_mongo=db_mongo)
-        status = UserApplicationStatus(name=_next_status, updated_by=current_user.id, date=datetime.now())
-        
+        status = UserApplicationStatus(
+            name=_next_status, updated_by=current_user.id, date=datetime.now(),
+        )
+
         new_status = await mobility_svc.add_status(
             db_mongo=db_mongo,
             new_status=status,
-            user_application_id=user_application_id
+            user_application_id=user_application_id,
         )
 
-        return "terminado"
+        return 'terminado'
 
     else:
         return next_status
