234cb2c2d334490810fe447fb946ccdaead13277
alwos19 <38044738+alwos19@users.noreply.github.com>
2024-11-20 07:24:45 -0500

    Feature/auth (#62)
    * creando los template para envio de correos
    
    * rosolciendo la activación de la cuenta
    
    * :sparkles: Se crea un servicio que se conecte a la API que usa activate-account. Se hace una página que muestra un botón que llama a ese servicio pero aún no se manda bien el token, saca error 422
    
    * activación de cliente funciona desde frontend, se envia la url del frontend desde el backend
    
    * Se envia correo de notificación al usuario creado
    
    * Refactor email service configuration
    
    * Refactor email service configuration
    
    * Tu mensaje de commit
    
    * Refactor pre-commit configuration
    
    * siga con pre-commit
    
    * Refactor email sending code and update docker-compose.dev.yml
    
    * Refactor email sending code and update configuration
    
    This commit refactors the email sending code in the `user.py` file and updates the configuration in the `config.py` file. It adds a new SMTP domain setting and updates the SMTP port setting. This change allows the email service to use the configured SMTP domain and port for sending emails.
    
    ---------
    
    Co-authored-by: jeronimoloaiza <jeronimo.loaiza@udea.edu.co>

--- app/infraestructure/services/emails/user.py
+++ app/infraestructure/services/emails/user.py
@@ -4,9 +4,7 @@ import smtplib
 from email.message import EmailMessage
 
 from jinja2 import Environment
-from jinja2 import FileSystemLoader
 
-from .templates import templatesdir
 from app.core.celery_worker import celery_app
 from app.core.config import settings
 from app.core.logging import logging
@@ -39,9 +37,16 @@ def confirm_email(to_name: str, token: str, email):
         subtype='html',
     )
 
-    with smtplib.SMTP_SSL('smtp.gmail.com', port=465) as smtp:
-        smtp.login(_my_email, _my_pwd)
-        smtp.send_message(msg)
+    # with smtplib.SMTP_SSL('smtp.gmail.com', port=465) as smtp:
+    #     smtp.login(_my_email, _my_pwd)
+    #     smtp.send_message(msg)
 
-    # with smtplib.SMTP("172.19.0.101", port=25) as smtp:
-    #    smtp.send_message(msg=msg, from_addr=_my_email, to_addrs= email)
+    with smtplib.SMTP(
+        settings.smtp_domain_email,
+        port=settings.smtp_port_email,
+    ) as smtp:
+        smtp.send_message(
+            msg=msg,
+            from_addr=_my_email,
+            to_addrs=email,
+        )
