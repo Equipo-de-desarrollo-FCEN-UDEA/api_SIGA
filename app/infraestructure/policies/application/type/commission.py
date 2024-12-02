c2b78c899006bbc8c07bf261191660de34d634a6
DavidTorres-sys <david.torresg@udea.edu.co>
2024-12-02 12:05:13 -0500

    Feature: Add next_status function for application status transitions and update ApplicationStatusType

---
+++ app/infraestructure/policies/application/type/commission.py
@@ -0,0 +1,44 @@
+from __future__ import annotations
+
+from app.infraestructure.policies.application.user_application import (
+    current_status,
+)
+from app.protocols.db.models.application.application import ApplicationStatusType
+from app.schemas.users.user import User
+
+
+def next_status(current_status: str, response: str | None = None) -> str | None:
+    """
+    Determines the next application status based on the current status and optional response.
+
+    Args:
+        current_status (str): The current status of the application.
+        response (str | None, optional): An optional response value that can influence the status transition.
+            - "APROBADA" (Approved) or "RECHAZADA" (Rejected) when the status is APPROVAL or IN_DEAN.
+            - Defaults to None if no response is given.
+
+    Returns:
+        str | None: The next application status based on the provided transition rules.
+            - Returns `None` if no valid transition exists for the given `current_status` or response.
+
+    """
+
+    status_transitions = {
+        ApplicationStatusType.CREATE.value: ApplicationStatusType.APPROVAL.value,
+        ApplicationStatusType.APPROVAL.value: {
+            'APROBADA': ApplicationStatusType.IN_DEAN.value,
+            'RECHAZADA': ApplicationStatusType.REJECTED.value,
+        },
+        ApplicationStatusType.IN_DEAN.value: {
+            'APROBADA': ApplicationStatusType.APPROVED.value,
+            'RECHAZADA': ApplicationStatusType.REJECTED.value,
+        },
+    }
+
+    if current_status not in status_transitions:
+        return None
+
+    if isinstance(status_transitions[current_status], dict) and response:
+        return status_transitions[current_status].get(response)
+
+    return status_transitions[current_status]
--- app/protocols/db/models/application/application.py
+++ app/protocols/db/models/application/application.py
@@ -1,19 +1,24 @@
+from __future__ import annotations
+
 from enum import Enum
-from app.protocols.db.utils.base_model import BaseModel
 from uuid import UUID
 
+from app.protocols.db.utils.base_model import BaseModel
+
+
 class ApplicationStatusType(Enum):
-    CREATE = "CREADA"
-    IN_INSTITUTE = "EN INSTITUTO"
-    IN_COMMITEE = "EN COMITE"
-    IN_INTERNATIONAL = "EN RELACIONES INTERNACIONALES"
-    REJECTED = "RECHAZADA"
-    RETURNED = "DEVUELTA"
-    IN_DEAN = "EN DECANATURA"
-    APPROVED = "APROBADA"
+    CREATE = 'CREADA'
+    IN_INSTITUTE = 'EN INSTITUTO'
+    APPROVAL = 'VISTO BUENO'
+    IN_COMMITEE = 'EN COMITE'
+    IN_INTERNATIONAL = 'EN RELACIONES INTERNACIONALES'
+    REJECTED = 'RECHAZADA'
+    RETURNED = 'DEVUELTA'
+    IN_DEAN = 'EN DECANATURA'
+    APPROVED = 'APROBADA'
+
 
 class Application(BaseModel):
     name: str
     description: str
     academic_unit_id: UUID
-
