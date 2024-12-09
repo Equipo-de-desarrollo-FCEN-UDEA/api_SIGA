a912f587d01edd939b5ca5cd4200e1f8f001e0d2
Esteban Durango <esteban.durangov@udea.edu.co>
2024-12-09 16:49:01 -0500

    feat: enhance Mobility model and CRUD operations with new fields and total_time calculation

--- app/infraestructure/db/models/application/type/mobility.py
+++ app/infraestructure/db/models/application/type/mobility.py
@@ -6,6 +6,7 @@ from uuid import UUID
 from odmantic import Field
 from odmantic import Model
 
+from app.schemas.application.type.mobility import Subject
 from app.schemas.application.user_application import UserApplicationStatus
 
 
@@ -16,8 +17,13 @@ class Mobility(Model):
     purpose: str
     destination_country: str
     destination_institution: str
+    academic_program: str
+    name_contact_person: str
+    cellphone_contact_person: str
+    email_contact_person: str
     date_start: datetime
     date_end: datetime
     total_time: int
-    date_report: datetime
+    date_report: datetime = Field(default_factory=datetime.utcnow)
+    subjects: list[Subject]
     status: list[UserApplicationStatus]
