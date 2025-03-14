41b30ca1f864749e59dff0c03556fb9074d474f8
estebandurangov <esteban.durangov@udea.edu.co>
2025-03-13 19:30:21 -0500

    Se crea endpoint para cargar los archivos de una solicitud

--- app/api/routes/v1/application/user_application.py
+++ app/api/routes/v1/application/user_application.py
@@ -7,10 +7,12 @@ from fastapi import APIRouter
 from fastapi import Depends
 from fastapi import UploadFile
 from fastapi.responses import JSONResponse
+from fastapi.responses import StreamingResponse
 from sqlalchemy.orm import Session
 
 from app.api.middleware.postgres_db import get_db
 from app.core.config import settings
+from app.infraestructure.services.aws.s3 import s3
 from app.schemas.application.user_application import UserApplicationPublic
 from app.services.application.type.purchase import purchase_svc
 from app.services.application.user_application import user_application_svc
@@ -53,3 +55,23 @@ async def upload_files(
         user_application_id=id, files=files, db=db_postgres,
     )
     return res
+
+
+@router.get('/get_document/')
+async def get_document(
+    *,
+    user_id: UUID,
+    user_application_id: UUID,
+    document_name: str,
+):
+    key: str = f'{user_id}/{user_application_id}/{document_name}'
+    try:
+        res = s3.get_data_from_s3_bucket(
+            bucket_name=settings.aws_bucket_name, file_name=key,
+        )
+    except Exception as e:
+        return JSONResponse(
+            status_code=400,
+            content={'error': f'Error: {str(e)}'},
+        )
+    return StreamingResponse(content=res['Body'], media_type=res['ContentType'])
