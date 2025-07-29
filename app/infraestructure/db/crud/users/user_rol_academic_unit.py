5ecab71f5ed413f5199e63cbf495de2c46ce74bc
jeronimoloaiza <124712953+jeronimoloaiza@users.noreply.github.com>
2025-07-29 15:53:58 -0500

    Feature/status migration (#155)
    * :bug: Se hace que se calcule automáticamente el comité al que van dirigidas las solicitudes en el flujo de estados
    
    * Eliminando código comentado

--- app/infraestructure/db/crud/users/user_rol_academic_unit.py
+++ app/infraestructure/db/crud/users/user_rol_academic_unit.py
@@ -28,6 +28,8 @@ class UserRolAcademicUnitCrud(
         ESTUDIANTE_PREGRADO_ROL_ID = UUID('939875b2-3e34-4a17-9f3c-76cabba73f52')
         ESTUDIANTE_POSGRADO_ROL_ID = UUID('1ca355db-8700-4ee7-883b-18b8bbed403b')
         TYPE_COMITE = UUID('d1085905-eed1-4e3c-bae6-9f3b17295eca')
+        TYPE_COMITE_PREGRADO = UUID('8e544f64-0cb5-4287-847a-23478a4edd79')
+        TYPE_COMITE_POSGRADO = UUID('0ccdafbb-32e8-41be-9ac8-78470762d7ae')
 
         # Realizamos una sola consulta para ambos roles
         user_rol = db.query(UserRolAcademicUnit).options(
@@ -45,15 +47,16 @@ class UserRolAcademicUnitCrud(
         # Lista de unidades académicas "hijas" del instituto, como los comités
         lista = user_rol.academic_unit.academic_units
         for academic_unit in lista:
+            if academic_unit.academic_unit_type_id == TYPE_COMITE:
+                return academic_unit.id
             if (
-                (
-                    user_rol.rol_id == ESTUDIANTE_PREGRADO_ROL_ID and
-                    academic_unit.academic_unit_type_id == TYPE_COMITE
-                ) or
-                (
-                    user_rol.rol_id == ESTUDIANTE_POSGRADO_ROL_ID and
-                    academic_unit.academic_unit_type_id == TYPE_COMITE
-                )
+                user_rol.rol_id == ESTUDIANTE_PREGRADO_ROL_ID and
+                academic_unit.academic_unit_type_id == TYPE_COMITE_PREGRADO
+            ):
+                return academic_unit.id
+            if (
+                user_rol.rol_id == ESTUDIANTE_POSGRADO_ROL_ID and
+                academic_unit.academic_unit_type_id == TYPE_COMITE_POSGRADO
             ):
                 return academic_unit.id
 
@@ -81,7 +84,12 @@ class UserRolAcademicUnitCrud(
 
         # Lista de unidades académicas "hijas" del instituto
         lista = user_rol.academic_unit.academic_units
-
+        for academic_unit in lista:
+            print(
+                '====================', academic_unit.id,
+                academic_unit.academic_unit_type_id,
+                academic_unit.name,
+            )
         for academic_unit in lista:
             if academic_unit.academic_unit_type_id == TYPE_CONSEJO:
                 return academic_unit.id
